5c596f6fb4c6001ab25d2536b380046d2b7394f6
Mo Azza <ssbsdmg@gmail.com>
2025-02-18 15:52:16 +0200

    Update page.tsx

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -43,9 +43,12 @@ export default function Page() {
   };
 
   return (
-    <div>
-      Contact an admin for early access.
-    </div>
+        <div className="flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16">
+          <h3 className="text-xl font-semibold dark:text-zinc-50">NeuralSky Access</h3>
+          <p className="text-sm text-gray-500 dark:text-zinc-400">
+            Contact an admin for early access to NeuralSky.
+          </p>
+        </div>
     // <div className="flex h-dvh w-screen items-start pt-12 md:pt-0 md:items-center justify-center bg-background">
     //   <div className="w-full max-w-md overflow-hidden rounded-2xl gap-12 flex flex-col">
     //     <div className="flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16">
